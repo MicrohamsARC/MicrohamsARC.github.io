d3bd7c6cd6b32aafb12c5694fd326a33af065487
radiolabme <brian@radiolab.me>
2025-06-17 03:00:34 -0700

    Remove custom layouts/includes now that Minimal Mistakes is in use

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Site Launch",
+        "excerpt":"We’re excited to launch the new MicroHAMS website! Stay tuned for updates and more events.  ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2025/06/01/launch/",
+        "teaser": null
+      }]
